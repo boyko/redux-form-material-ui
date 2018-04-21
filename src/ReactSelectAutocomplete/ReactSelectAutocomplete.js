7706f14875a5660e088bf4c66627e56cc4495873
amarov <boyko.amarov@gmail.com>
2018-04-21 14:18:46 +0300

    Add test setup. Add react-select.

---
+++ src/ReactSelectAutocomplete/ReactSelectAutocomplete.js
@@ -0,0 +1,78 @@
+/* eslint-disable react/prop-types */
+
+import React from "react";
+import TextField from "material-ui/TextField";
+import Select from "react-select";
+
+function SelectWrapped(props) {
+  const {
+    classes,
+    noResultsText,
+    arrowRenderer,
+    clearRenderer,
+    valueComponent,
+    joinValues,
+    ...other
+  } = props;
+
+  return (
+    <Select
+      optionComponent={Option}
+      noResultsText={noResultsText}
+      arrowRenderer={arrowRenderer}
+      clearRenderer={clearRenderer}
+      valueComponent={valueComponent}
+      joinValues={joinValues}
+      {...other}
+    />
+  );
+}
+
+class ReactSelectAutocomplete extends React.Component {
+  render() {
+    const {
+      classes,
+      value,
+      onChange,
+      optionComponent,
+      valueComponent,
+      clearRenderer,
+      arrowRenderer,
+      options,
+      placeholder,
+      multi,
+      clearable,
+      noResultsText,
+      ...rest
+    } = this.props;
+
+    return (
+      <TextField
+        fullWidth
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+        InputProps={{
+          inputComponent: Select,
+          inputProps: {
+            classes,
+            optionComponent,
+            valueComponent,
+            clearRenderer,
+            arrowRenderer,
+            noResultsText,
+            name: "react-select-single",
+            instanceId: "react-select-single",
+            simpleValue: true,
+            multi,
+            joinValues: true, // Due to bug with react-select refs
+            options
+          }
+        }}
+        {...rest}
+      />
+    );
+  }
+}
+
+export default ReactSelectAutocomplete;

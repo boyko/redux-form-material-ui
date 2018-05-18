2aca0fa76a4e53724ea25fd735ba2758fbbfab61
amarov <boyko.amarov@gmail.com>
2018-05-18 20:14:54 +0300

    Fix errors with material-ui v1 migration.

--- src/ReactSelectAutocomplete/MuiReactSelectAutocompleteStyled.js
+++ src/ReactSelectAutocomplete/MuiReactSelectAutocompleteStyled.js
@@ -1,17 +1,17 @@
 /* eslint-disable react/prop-types */
 
-import React from "react";
-import { withStyles } from "material-ui/styles";
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
 // import Typography from '@material-ui/core/Typography';
-import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
-import ArrowDropUpIcon from "@material-ui/icons/ArrowDropUp";
-import ClearIcon from "@material-ui/icons/Clear";
-import styles from "./styles";
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import ArrowDropUpIcon from '@material-ui/icons/ArrowDropUp';
+import ClearIcon from '@material-ui/icons/Clear';
+import styles from './styles';
 
-import MuiReactSelectAutocomplete from "./MuiReactSelectAutocomplete";
-import ValueComponent from "./ValueComponent";
-import OptionComponent from "./OptionComponent";
-import ArrowRenderer from "./ArrowRenderer";
+import MuiReactSelectAutocomplete from './MuiReactSelectAutocomplete';
+import ValueComponent from './ValueComponent';
+import OptionComponent from './OptionComponent';
+import ArrowRenderer from './ArrowRenderer';
 
 const ClearRenderer = () => <ClearIcon/>;
 // const NoResultsText = () => <Typography>{'No results found'}</Typography>;
@@ -19,11 +19,11 @@ const ClearRenderer = () => <ClearIcon/>;
 const MaterialUiReactSelect = props => (<MuiReactSelectAutocomplete {...props} />);
 
 MaterialUiReactSelect.defaultProps = {
-  optionComponent: OptionComponent,
-  valueComponent: ValueComponent,
-  clearRenderer: ClearRenderer,
-  arrowRenderer: ArrowRenderer,
-  noResultsText: "No results found"
+    optionComponent: OptionComponent,
+    valueComponent: ValueComponent,
+    clearRenderer: ClearRenderer,
+    arrowRenderer: ArrowRenderer,
+    noResultsText: 'No results found',
 };
 
 export default withStyles(styles)(MaterialUiReactSelect);
